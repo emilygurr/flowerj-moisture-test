ab96cd2da973bb01b7d93e056074b42a37386329
emilygurr <97482488+emilygurr@users.noreply.github.com>
2022-04-27 15:23:21 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,17 +8,31 @@ dfplayer.setVolume(18)
 pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
 basic.forever(function () {
     buttontouch = pins.digitalReadPin(DigitalPin.P2)
-    moistureReading = pins.digitalReadPin(DigitalPin.P7)
+    moistureReading = pins.digitalReadPin(DigitalPin.P8)
     if (buttontouch == 1) {
         serial.writeValue("value", 1)
         basic.pause(500)
-        if (moistureReading <= 150) {
+        if (moistureReading <= 75) {
             dfplayer.MP3_setSerial(SerialPin.P1, SerialPin.P0)
-            dfplayer.folderPlay(2, 2, dfplayer.yesOrNot.type1)
+            dfplayer.folderPlay(1, 4, dfplayer.yesOrNot.type1)
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
         }
-        if (moistureReading >= 151) {
+        if (moistureReading >= 76) {
             dfplayer.MP3_setSerial(SerialPin.P1, SerialPin.P0)
-            dfplayer.folderPlay(1, 1, dfplayer.yesOrNot.type1)
+            dfplayer.folderPlay(1, 3, dfplayer.yesOrNot.type1)
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                # . . . #
+                # . . . #
+                . # # # .
+                `)
         }
     } else {
         serial.writeString("not")
